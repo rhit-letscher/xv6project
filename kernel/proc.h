0f3768739eb4857fe60ef2d2b769292aa8a28620
Letscher <letscher@rose-hulman.edu>
2024-05-07 12:54:33 -0400

    lol

--- kernel/proc.h
+++ kernel/proc.h
@@ -89,6 +89,7 @@ struct sthread {
   void* arg;
   struct trapframe *trapframe;
   uint64 kstack;
+  int tid;                     //Thread id
 }
 
 // Per-process state
@@ -104,7 +105,6 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
-  int tid;                     //Thread id
 
   //pointer to main function so we can init
   void* (* func) mainfunc;
@@ -125,9 +125,4 @@ struct proc {
   char name[16];               // Process name (debugging)
 };
 
-struct thread_t {
-  struct proc threadproc;
-  struct trapframe *trapframe;
-  uint64 kstack;
 
-}
